7496280ec6ae6a209bbc9d0c05a06f94a9662491
jefri <jefri@gmail.com>
2025-02-17 20:01:59 -0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident molestiae quisquam, temporibus fuga quae eligendi repellendus voluptate et libero, officia voluptates rerum? Fugit eum, maiores aliquam perspiciatis inventore libero. Molestias!</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
